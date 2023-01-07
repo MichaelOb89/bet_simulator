c2e30e348ce2e0bbe403410dcb6d47c4626ed237
MichaelOb89 <michaelobrien.t@hotmail.com>
2023-01-07 14:31:58 -0500

    Payout function

--- config/betPayout.js
+++ config/betPayout.js
@@ -2,18 +2,14 @@ require('dotenv').config()
 const db = require('./database')
 const Bet = require('../models/bet')
 const pastGames = require('../data/finishedGames').response;
-const { response } = require('../data/finishedGames');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const payout = async () => {
     const data = await fetch('http://localhost:3001/api/bets')
     const bets = await data.json()
-    // console.log(bets)
     bets.forEach(bet => {
-        const matchId = bet.matchId
         const foundGame = pastGames.find(game=>game.fixture.id==bet.matchId)
-        // console.log(foundGame)
-        console.log(bet._id)
+        // updateBetWinner(bet, foundGame)
         if(bet.team == "Draw" && (!foundGame.teams.home.winner && !foundGame.teams.away.winner)){
             console.log("Won bet!")
             Bet.findByIdAndUpdate(bet._id, {won: true}).exec()
